7012a1e2f417c1408461ee5cd7d7d03409ab8667
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2025-08-14 19:35:17 +0600

    Added login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Sidebar from "./components/sidebar/Sidebar";
 import Homepage from "./pages/Homepage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
-    <div className="flex min-h-screen w-full">
-      <aside className="w-[18%] min-w-[240px] max-w-[260px]">
-        <Sidebar />
-      </aside>
-
-      <main className="flex-1 bg-background overflow-y-auto">
-        <Homepage />
-      </main>
-    </div>
+    <>
+      <Routes>
+        <Route element={<Homepage />} path="/" />
+        <Route element={<LoginPage />} path="/login" />
+      </Routes>
+    </>
   );
 }
 
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -5,6 +5,7 @@ import StatsCards from "../components/homePage/StatsCards";
 import { useAgencyStore } from "../store/AgencyStore";
 import LeadsByAgency from "../components/homePage/LeadsByAgency";
 import Copyright from "../components/Copyright";
+import Sidebar from "../components/sidebar/Sidebar";
 
 const Homepage = () => {
   const { fetchAgencies } = useAgencyStore();
@@ -14,18 +15,26 @@ const Homepage = () => {
   }, []);
 
   return (
-    <div className="">
-      <Header />
-      <div className="px-4 pt-4">
-        <p className="text-lg md:text-2xl 2xl:text-[30px] font-bold">
-          Dashboard
-        </p>
-        <StatsCards />
-        <LeadsByAgency />
-        <AgencySection />
-      </div>
+    <div className="flex min-h-screen w-full">
+      <aside className="w-[18%] min-w-[240px] max-w-[260px]">
+        <Sidebar />
+      </aside>
 
-      <Copyright />
+      <main className="flex-1 bg-background overflow-y-auto">
+        <div className="">
+          <Header />
+          <div className="px-4 pt-4">
+            <p className="text-lg md:text-2xl 2xl:text-[30px] font-bold">
+              Dashboard
+            </p>
+            <StatsCards />
+            <LeadsByAgency />
+            <AgencySection />
+          </div>
+
+          <Copyright />
+        </div>
+      </main>
     </div>
   );
 };
